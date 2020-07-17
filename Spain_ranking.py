3e254fbb0342913e36ed70e493be28d351a065a7
vbrunsch <62330755+vbrunsch@users.noreply.github.com>
2020-07-16 22:36:38 -0400

    Update Spain_ranking.py

--- Spain_ranking.py
+++ Spain_ranking.py
@@ -12,7 +12,7 @@ ab['Abbrev'].fillna('NA', inplace = True)
 
 focus = df.copy().drop(['num_casos_prueba_pcr','num_casos_prueba_test_ac','num_casos_prueba_otras','num_casos_prueba_desconocida'], axis=1).set_index(['fecha'])
 confirm = focus.groupby('provincia_iso').sum().T
-#print(confirm)
+
 cols=['Province','COVID-Free Days','New Cases in Last 14 Days']
 collect = []
 for p in confirm.columns:
