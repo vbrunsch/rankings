8f9707e841326797107dc2b89f9b35b46197aa1f
vbrunsch <62330755+vbrunsch@users.noreply.github.com>
2020-09-28 01:09:00 -0400

    Create Japan_ranking.py

---
+++ Japan/Japan_ranking.py
@@ -0,0 +1,168 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import pandas as pd
+
+df = pd.read_json('https://github.com/reustle/covid19japan-data/raw/master/docs/patient_data/2020-09-27.json')
+df = df[df['confirmedPatient']==True]
+focus = df.drop(['patientId', 'ageBracket', 'gender', 'patientStatus', 'notes', 'mhlwPatientNumber', 'prefecturePatientNumber', 'prefectureSourceURL', 'confirmedPatient', 'residence', 'sourceURL', 'relatedPatients', 'knownCluster', 'charterFlightPassenger', 'cityPrefectureNumber', 'citySourceURL', 'deceasedDate', 'deceasedReportedDate', 'deathSourceURL', 'cruisePassengerDisembarked'], axis = 1)#.set_index('dateAnnounced')
+focus['new'] = 1
+pref_only = focus.groupby('detectedPrefecture').sum().T
+pref_only = pref_only.drop(['Unspecified','Port Quarantine'], axis = 1)
+pref= focus.groupby(['dateAnnounced','detectedPrefecture']).sum()
+pref = pref.reset_index(level=1)
+pref.index = pd.to_datetime(pref.index, dayfirst=True)
+tim = pref.index.max()
+idx = pd.date_range('01-24-2020', tim)
+
+
+cols=['Prefectures','COVID-Free Days','New Cases in Last 14 Days', 'Last7', 'Previous7']
+collect = []
+
+for d in pref_only.columns:
+    cas = pref[pref['detectedPrefecture']==d].drop(['detectedPrefecture'], axis = 1)
+    cas = cas.reindex(idx, fill_value=0)
+    ave = cas['new']
+    las = len(ave)-14
+    last_forteen = int(ave[las:].sum().item())
+    if last_forteen < 0:
+        last_forteen = 0
+    last7 = int(ave[len(ave)-7:].sum().item()) #last week
+    prev7 = int(ave[len(ave)-14:len(ave)-7].sum().item()) #prev week
+    if last7 < 0:
+        last7 = 0
+    if last7 > last_forteen:
+        last_forteen = last7
+    if prev7 < 0:
+        prev7 = 0
+    if (last7 == 0) & (last_forteen == 0):
+        prev7 = 0
+    i = len(ave)-1
+    c = 0
+    while i > 0:
+        if ave[i] <= 0:
+            c = c + 1
+        else:
+            i = 0
+        i = i - 1
+
+    collect.append((d,
+                   c,
+                   last_forteen,
+                   last7,
+                   prev7))
+
+    
+thr = pd.DataFrame(collect, columns=cols)
+fin = thr.sort_values(['COVID-Free Days'], ascending=[False])
+fin['week'] = fin['COVID-Free Days'].gt(13) 
+tab = fin.sort_values(['week'], ascending=[False])
+tab_t = tab[tab['week']==True]
+tab_f = tab[tab['week']==False]
+tab_f = tab_f.sort_values(['New Cases in Last 14 Days','COVID-Free Days'], ascending = [True,False])
+tab_t = tab_t.sort_values(['COVID-Free Days','New Cases in Last 14 Days'], ascending = [False,True])
+tab = tab_t.append(tab_f)
+tab = tab.drop(['week'], axis=1)
+
+#Percent Change
+
+tab['PercentChange'] = 100*(tab['Last7'] - tab['Previous7'])/(tab['Last7']+tab['Previous7'])
+tab['PercentChange'] = tab['PercentChange'].fillna(0.0)
+
+tab = tab.drop(['Previous7'], axis = 1)
+tab.columns = ['Prefectures', 'COVID-Free Days', 'New Cases in Last 14 Days', 'Last 7 Days', 'Pct Change']
+
+def highlighter(s):
+    val_1 = s['COVID-Free Days']
+    val_2 = s['New Cases in Last 14 Days']
+    
+    r=''
+    try:
+        if val_1>=14: #More than 14 Covid free days
+            r = 'background-color: #018001; color: #ffffff;'
+        elif 20>=val_2 : # less than 20 in last 2 weeks
+            r = 'background-color: #02be02; color: #ffffff;'
+        elif 200>=val_2 >=21: #Light green
+            r = 'background-color: #ffff01;'
+        elif 1000>=val_2 >= 201: #Yellow
+            r = 'background-color: #ffa501;'
+        elif 20000>=val_2 >= 1001: #Orange
+            r = 'background-color: #ff3434;'
+        elif val_2 > 20001: # Red
+            r = 'background-color: #990033;'
+    except Exception as e:
+        r = 'background-color: white'
+    return [r]*(len(s)-2) + ['']*2
+
+def hover(hover_color="#ffff99"):
+    return dict(selector="tbody tr:hover td, tbody tr:hover th",
+                props=[("background-color", "rgba(66, 165, 245, 0.2) !important")])
+
+top = """
+<!DOCTYPE html>
+<meta content="text/html;charset=utf-8" http-equiv="Content-Type">
+<meta content="utf-8" http-equiv="encoding">
+<html>
+<head>
+
+<style>
+
+    h2 {
+        text-align: center;
+        font-family: Helvetica, Arial, sans-serif;
+    }
+    table { 
+        margin-left: auto;
+        margin-right: auto;
+    }
+    table, th, td {
+        border: 1px solid black;
+        border-collapse: collapse;
+    }
+    th, td {
+        padding: 5px;
+        text-align: center;
+        font-family: Helvetica, Arial, sans-serif;
+        font-size: 90%;
+    }
+    table tbody tr:hover {
+        background-color: #dddddd;
+    }
+    /*
+    table tbody tr:hover td, table tbody tr:hover th {
+  background-color: #dddddd !important;
+    }
+    /*
+    .wide {
+        width: 90%; 
+    }
+
+</style>
+</head>
+<body>
+"""
+bottom = """
+</body>
+</html>
+"""
+
+arrow = lambda x : ' &#x2197;' if x>0 else (' &#x2192' if x ==0  else ' &#x2198')
+styles=[hover(),]
+tab['Rank'] = tab.reset_index().index
+tab['Rank'] = tab['Rank'].add(1)
+#tab.loc[tab['COVID-Free Days']==idx.size,['Rank']] = 1 
+tab['Trend'] = tab['Pct Change'].map(arrow)
+tab['Percent Change'] = tab['Pct Change'].map('{:,.2f}%'.format) + tab['Trend']
+tab = tab.drop(['Trend','Pct Change'], axis = 1)
+
+tab = tab[['Rank', 'Prefectures', 'COVID-Free Days', 'New Cases in Last 14 Days','Last 7 Days','Percent Change']]       
+s = tab.style.apply(highlighter, axis = 1).set_table_styles(styles).hide_index()
+
+try:        
+    with open(f'Japan_Prefectures.html', 'w', encoding="utf-8") as out:
+        body = s.render().replace('&#x2197;','<span style="color: red"> &#x2197;</span>') # red arrow up
+        body = body.replace('&#x2198','<span style="color: green"> &#x2198;</span>') # green arrow down
+        content = top + body + bottom
+        out.write(content)
+except Exception as e:
+    print(f'Error:\n{e}')
