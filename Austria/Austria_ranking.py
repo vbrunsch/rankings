b7836ba33d0aef0e67d733304d4845764529eb0f
vbrunsch <62330755+vbrunsch@users.noreply.github.com>
2020-11-06 21:34:08 -0500

    Update Austria_ranking.py

--- Austria/Austria_ranking.py
+++ Austria/Austria_ranking.py
@@ -12,14 +12,16 @@ import pandas as pd
 
 bez = pd.DataFrame()
 now = datetime.date.today()
+yes = now - datetime.timedelta(days=1)
 date_time = now.strftime("%Y%m%d")
+date_time_y = yes.strftime("%Y%m%d")
 print(date_time)
 try:
-    url = f"https://github.com/statistikat/coronaDAT/raw/master/archive/{date_time}/data/{date_time}_000200_orig_csv_gesundheitsministerium.zip"
+    url = f"https://github.com/statistikat/coronaDAT/raw/master/archive/{date_time}/data/{date_time}_140202_orig_csv_ages.zip"
     resp = urlopen(url)
 except:
     try:
-        url = f"https://github.com/statistikat/coronaDAT/raw/master/archive/{date_time}/data/{date_time}_070200_orig_csv_gesundheitsministerium.zip"
+        url = f"https://github.com/statistikat/coronaDAT/raw/master/archive/{date_time_y}/data/{date_time_y}_140202_orig_csv_ages.zip"
         resp = urlopen(url)
     except:
         pass
