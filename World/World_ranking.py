ce10de6e95a3a64f310270141ccb3b66e298424e
vbrunsch <62330755+vbrunsch@users.noreply.github.com>
2020-12-21 22:44:18 -0500

    Update World_ranking.py

--- World/World_ranking.py
+++ World/World_ranking.py
@@ -103,18 +103,25 @@ for j, country in enumerate(confirm.iloc[-1].sort_values(ascending=False).index[
         #    df_t[df_t.columns[6]][c] = dayt + datetime.timedelta(days=i)
         #    c=c+1
         df_t['announce_date'] = df_t['announce_date'].astype(str).replace({'[0-9][0-9][0-9][0-9]':'2020'},regex=True)
+        df_t['announce_date'] = df_t['announce_date'].astype(str).replace({'15/15':'15/12'},regex=True)
         df_t = df_t.set_index([df_t.columns[6]])
         df_t.index.name = None
-        df_t = df_t[df_t[df_t.columns[3]]=='Thailand']
-        df_t['new'] = 0
-        df_t.loc[pd.isna(df_t[df_t.columns[8]]),'new'] = 1
+        #df_t = df_t[df_t[df_t.columns[3]]=='Thailand']
+        df_t['new'] = 1
+        #df_t.loc[pd.isna(df_t[df_t.columns[8]]),'new'] = 1
+
+        df_t.loc[df_t[df_t.columns[8]]=='ผู้ที่เดินทางมาจากต่างประเทศ และเข้า OQ','new'] = 0
+        df_t.loc[df_t[df_t.columns[8]]=='ผู้ที่เดินทางมาจากต่างประเทศ และเข้า ASQ/ALQ','new'] = 0
+        df_t.loc[df_t[df_t.columns[8]]=='State Quarantine','new'] = 0
+        df_t.loc[df_t[df_t.columns[8]]=='คนต่างชาติเดินทางมาจากต่างประเทศ','new'] = 0
+
+
         tod = pd.to_datetime('today')
         idx = pd.date_range('01-22-2020', tod)
         df_t = df_t.groupby(df_t.index).sum()
         df_t.index = pd.to_datetime(df_t.index, dayfirst=True)
+        df_t = df_t[1:]
         focus = df_t.reindex(idx, fill_value=0)
-        man1 = pd.to_datetime('2020-10-13')
-        focus.loc[man1,'new'] = 3
    
     #correcting country names
     if country == 'Taiwan*':
