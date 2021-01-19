0d307ddaf91e8c2ad3b449de32524a6cd3caea54
vbrunsch <62330755+vbrunsch@users.noreply.github.com>
2021-01-18 20:38:32 -0500

    Update test.py

--- Test/test.py
+++ Test/test.py
@@ -3,5 +3,5 @@ import pandas as pd
 import numpy as np
 import geopandas as gpd
 
-df = gpd.read_file('https://opendata.arcgis.com/datasets/dd4580c810204019a7b8eb3e0b329dd6_0.gdb')
+df = gpd.read_file('zip://https://opendata.arcgis.com/datasets/dd4580c810204019a7b8eb3e0b329dd6_0.gdb')
 print(df)
