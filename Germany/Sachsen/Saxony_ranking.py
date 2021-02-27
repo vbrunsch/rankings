4bb4b9ebc3320043f3f6bcfecfb938ff3451d238
vbrunsch <62330755+vbrunsch@users.noreply.github.com>
2021-02-26 21:27:55 -0500

    Update Saxony_ranking.py

--- Germany/Sachsen/Saxony_ranking.py
+++ Germany/Sachsen/Saxony_ranking.py
@@ -106,8 +106,8 @@ df = df.set_index('Gemeinde')
 df = df.iloc[:,1]
 #df = df['Neuzugänge letzten 7 Tage']
 print(df)
-with open(f'Germany/Sachsen/data/Sachsen_Staedte_for_rankings_{da}.csv'):
-    
+newfile = open(f'Germany/Sachsen/data/Sachsen_Staedte_for_rankings_{da}.csv','w')
+newfile.close()    
 df.to_csv(f'Germany/Sachsen/data/Sachsen_Staedte_for_rankings_{da}.csv')
 
 from datetime import datetime, timedelta
