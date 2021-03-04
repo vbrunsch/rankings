6a1424cdef7d12f7475ca4a394a5978207e3b6e0
aochen-jli <aochen.jli@gmail.com>
2021-03-03 20:42:42 -0800

    added page title

--- visualizations/app.py
+++ visualizations/app.py
@@ -1,5 +1,7 @@
 import os
 import yaml
+from bokeh.io import curdoc
+
 from visualizations.layout import VisualizationLayout
 
 # Read and start visualization server with yaml config
@@ -7,3 +9,4 @@ with open(os.getenv("CONFIG_PATH"), 'r') as stream:
     config_data = yaml.safe_load(stream)
 for vis_config in config_data['visualizations']:
     VisualizationLayout(**vis_config[list(vis_config.keys())[0]]).add_to_curdoc()
+curdoc().title = config_data.get("page_title", "Green Zone Visualizations")
