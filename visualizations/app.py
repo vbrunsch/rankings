de9a98c927c8738c4ecd75b45a46b0a16dfc9ee6
aochen-jli <aochen.jli@gmail.com>
2021-03-04 22:50:01 -0800

    added spacers/margins to layout

--- visualizations/app.py
+++ visualizations/app.py
@@ -1,12 +1,23 @@
 import os
 import yaml
 from bokeh.io import curdoc
+from bokeh.layouts import row
+from bokeh.models import Spacer
 
 from visualizations.layout import VisualizationLayout
 
 # Read and start visualization server with yaml config
 with open(os.getenv("CONFIG_PATH"), 'r') as stream:
     config_data = yaml.safe_load(stream)
-for vis_config in config_data['visualizations']:
-    VisualizationLayout(**vis_config[list(vis_config.keys())[0]]).add_to_curdoc()
+# Render with horizontal spacers on the sides and vertical spacers in between visualizations
+horizontal_margin = config_data.get("horizontal_margin", 25)
+between_margin = config_data.get("between_margin", 25)
+for i, vis_config in enumerate(config_data['visualizations']):
+    curdoc().add_root(
+        row(Spacer(width=horizontal_margin),
+            VisualizationLayout(**vis_config[list(vis_config.keys())[0]]).generate(),
+            Spacer(width=horizontal_margin)))
+    if i < (len(config_data['visualizations']) - 1):
+        curdoc().add_root(Spacer(height=between_margin))
 curdoc().title = config_data.get("page_title", "Green Zone Visualizations")
+
