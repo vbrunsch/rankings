74f568ba151839427e63ac23170b0cea8444f263
vbrunsch <62330755+vbrunsch@users.noreply.github.com>
2021-03-25 22:05:49 -0400

    Update Worms.py

--- Germany/RP/Worms/Worms.py
+++ Germany/RP/Worms/Worms.py
@@ -10,6 +10,7 @@ from bs4 import BeautifulSoup
 #import requests
 import pandas as pd
 
+
 import re
 
 from datetime import timedelta
@@ -21,12 +22,12 @@ kon = pd.DataFrame()
 zus = pd.DataFrame()
 we = 0
 
-for x in range(0,13):
+for x in range(1,14):
     x = x + we*2
     m = 0
     tod = pd.Timestamp.today() -timedelta(days=x)
     tod = tod.strftime('%d.%m.%Y')
-    while m == 0 and x < 14:
+    while m == 0 and x < 15:
         for url in ['https://www.kreis-alzey-worms.eu/verwaltung/aktuelles/','https://www.kreis-alzey-worms.eu/verwaltung/aktuelles/?pageId586a9b2b=2#list_586a9b2b','https://www.kreis-alzey-worms.eu/verwaltung/aktuelles/?pageId586a9b2b=3#list_586a9b2b','https://www.kreis-alzey-worms.eu/verwaltung/aktuelles/?pageId586a9b2b=4#list_586a9b2b','https://www.kreis-alzey-worms.eu/verwaltung/aktuelles/?pageId586a9b2b=5#list_586a9b2b','https://www.kreis-alzey-worms.eu/verwaltung/aktuelles/?pageId586a9b2b=6#list_586a9b2b']:
             html = urllib.request.urlopen(url)
             htmlParse = BeautifulSoup(html, 'html.parser')
@@ -40,7 +41,7 @@ for x in range(0,13):
                         print(matches[0])
                         lin = re.findall('href="(.*)">Corona', str(para))
                         link = 'https://www.kreis-alzey-worms.eu/'+ lin[0]
-                        df = pd.read_html(link)
+                        df = pd.read_html(link, encoding = 'utf-8')
                         df = df[0]
                         df = df.replace('-', 0)
                         df[df.columns[1]] = df[df.columns[1]].astype(int)
@@ -196,9 +197,9 @@ for x in range(0,13):
                             kon = pd.DataFrame(index = df.index)
                             zus = pd.DataFrame(index = df.index)
 
-                        neu[tod] = cop[['Neue FÃ¤lle']]
+                        neu[tod] = cop[['Neue Fälle']]
                         unb[tod] = cop[['unbekannter Infektionsherd']]
-                        rei[tod] = cop[['ReiserÃ¼ckkehrer']]
+                        rei[tod] = cop[['Reiserückkehrer']]
                         kon[tod] = cop[['Kontakt zu positiv getesteter Person']]
                         
                         if re.findall('Corona: Seit',t):
@@ -215,6 +216,7 @@ for x in range(0,13):
                             unb[tod] = 0
                             rei[tod] = 0
                             kon[tod] = 0
+                        
 import numpy as np
 
 
@@ -228,5 +230,149 @@ zus['last14_risk'] = zus['last7'] + unb[unb.columns[7]]+unb[unb.columns[8]]+unb[
 zus['mix_risk'] = np.where(zus['last7_risk'] == 0, 0.6, zus['last7_risk'])
 zus['mix_risk'] = np.where(zus['last14_risk'] == 0, 0.2, zus['mix_risk'])
 
+
 zus.to_csv(f'Germany/RP/Worms/data/Worms_for_dw14_7.csv')
-print(zus)
+print(zus)      
+
+# For Rankings
+mdf = zus.copy()
+mdf['Gemeinde'] = cop[['Unnamed: 0']]
+mdf = mdf.drop_duplicates()
+mdf['Neuzugänge letzten 7 Tage_x'] = mdf['last7']
+mdf['Neuzugänge letzten 14 Tage'] = mdf['last14']
+mdf['Neuzugänge letzten 7 Tage_y'] = mdf['Neuzugänge letzten 14 Tage']-mdf['Neuzugänge letzten 7 Tage_x']  
+mdf['Covid-freie Wochen'] = 0
+mdf['Covid-freie Wochen'] = np.where(mdf['Neuzugänge letzten 7 Tage_x'] == 0, 1, mdf['Covid-freie Wochen'])
+mdf['Covid-freie Wochen'] = np.where(mdf['Neuzugänge letzten 14 Tage'] == 0 , 2, mdf['Covid-freie Wochen'])
+mdf = mdf[['Gemeinde','Covid-freie Wochen','Neuzugänge letzten 14 Tage','Neuzugänge letzten 7 Tage_x','Neuzugänge letzten 7 Tage_y']]
+#print(mdf)
+
+cols=['Gemeinde','Covid-freie Wochen','Neuzugänge letzten 14 Tage','Neuzugänge letzten 7 Tage_x','Neuzugänge letzten 7 Tage_y']
+thr = pd.DataFrame(mdf, columns=cols)
+fin = thr.sort_values(['Covid-freie Wochen'], ascending=[False])
+fin['week'] = fin['Covid-freie Wochen'].gt(13) 
+tab = fin.sort_values(['week'], ascending=[False])
+tab_t = tab[tab['week']==True]
+tab_f = tab[tab['week']==False]
+tab_f = tab_f.sort_values(['Neuzugänge letzten 7 Tage_x','Neuzugänge letzten 14 Tage'], ascending = [True,True])
+tab_t = tab_t.sort_values(['Covid-freie Wochen','Neuzugänge letzten 7 Tage_x'], ascending = [False,True])
+tab = tab_t.append(tab_f)
+tab = tab.drop(['week'], axis=1)
+
+#Percent Change
+
+tab['PercentChange'] = 100*(tab['Neuzugänge letzten 7 Tage_x'] - tab['Neuzugänge letzten 7 Tage_y'])/(tab['Neuzugänge letzten 7 Tage_x']+tab['Neuzugänge letzten 7 Tage_y'])
+tab['PercentChange'] = tab['PercentChange'].fillna(0.0)
+
+tab = tab.drop(['Neuzugänge letzten 7 Tage_y'], axis = 1)
+#tab.columns = ['Gemeinde', 'Covid-freie Wochen', 'Neue Fälle letzte 14 Tage', 'Letzte 7 Tage', 'Pct Change']
+
+def highlighter(s):
+    val_1 = s['Letzte 7 Tage']
+    val_2 = s['Neue Fälle letzte 14 Tage']
+    
+    r=''
+    try:
+        if 0==val_2: #More than 2 Covid free weeks
+            r = 'background-color: #24773B; color: #ffffff;'
+        elif 0==val_1 : # less than 20 in last 2 weeks
+            r = 'background-color: #89c540;' 
+        elif 10>=val_1 >=1: #Yellow
+            r = 'background-color: #f9cc3d;'
+        elif 35>=val_1 >= 11: #Orange
+            r = 'background-color: #f8961d;'
+        elif 100>=val_1 >= 36: #Light Red
+            r = 'background-color: #ef3d23;'
+        elif 1000>=val_1 > 100: # Red
+            r = 'background-color: #B11F24;'
+        elif 10000>=val_1 > 1000: # Light Purple
+            r = 'background-color: #652369;'
+        elif val_1 > 10000: # Purple
+            r = 'background-color: #36124B; color: #ffffff;'
+    except Exception as e:
+        r = 'background-color: white'
+    if 0 == val_2:
+        return [r]*(len(s)-2) + ['']*2
+    else:
+        return [r]*(len(s)-3) + [''] + [r] + ['']
+
+def hover(hover_color="#ffff99"):
+    return dict(selector="tbody tr:hover td, tbody tr:hover th",
+                props=[("background-color", "rgba(66, 165, 245, 0.2) !important")])
+
+top = """
+<!DOCTYPE html>
+<meta content="text/html;charset=utf-8" http-equiv="Content-Type">
+<meta content="utf-8" http-equiv="encoding">
+<html>
+<head>
+<style>
+    h2 {
+        text-align: center;
+        font-family: Helvetica, Arial, sans-serif;
+    }
+    table { 
+        margin-left: auto;
+        margin-right: auto;
+    }
+    table, th, td {
+        border: 1px solid black;
+        border-collapse: collapse;
+    }
+    th, td {
+        padding: 5px;
+        text-align: center;
+        font-family: Helvetica, Arial, sans-serif;
+        font-size: 90%;
+    }
+    table tbody tr:hover {
+        background-color: #dddddd;
+    }
+    /*
+    table tbody tr:hover td, table tbody tr:hover th {
+  background-color: #dddddd !important;
+    }
+    /*
+    .wide {
+        width: 90%; 
+    }
+</style>
+</head>
+<body>
+"""
+bottom = """
+</body>
+</html>
+"""
+
+arrow = lambda x : ' &#x2197;' if x>0 else (' &#x2192' if x ==0  else ' &#x2198')
+styles=[hover(),]
+tab['Platz'] = tab.reset_index().index
+tab['Platz'] = tab['Platz'].add(1)
+tab['Platz'] = np.where(tab['Neuzugänge letzten 14 Tage'] == 0 , 1, tab['Platz'])
+tab['Trend'] = tab['PercentChange'].map(arrow)
+tab['Percent Change'] = tab['PercentChange'].map('{:,.2f}%'.format) + tab['Trend']
+tab = tab.drop(['Trend','PercentChange'], axis = 1)
+
+tab.rename(columns = {'Neuzugänge letzten 14 Tage':'Neue Fälle letzte 14 Tage'}, inplace = True)
+tab.rename(columns = {'Neuzugänge letzten 7 Tage_x':'Letzte 7 Tage'}, inplace = True)
+tab.rename(columns = {'Percent Change':'Trend'}, inplace = True)
+tab.rename(columns = {'Gemeinde':'Stadt/Gemeinde'}, inplace = True)
+
+
+tab = tab[['Platz', 'Stadt/Gemeinde', 'Covid-freie Wochen', 'Neue Fälle letzte 14 Tage', 'Letzte 7 Tage','Trend']]
+tab = tab.drop('Covid-freie Wochen', axis = 1)
+s = tab.style.apply(highlighter, axis = 1).set_table_styles(styles).hide_index()
+
+import time
+toti = time.strftime('%m/%d/%Y %H:%M:%S')
+toti = "<center><caption>Last Update: " + toti + " UTC</caption></center>"
+
+try:        
+    with open(f'Worms.html', 'w', encoding="utf-8") as out:
+        body = s.render().replace('&#x2197;','<span style="color: red"> &#x2197;</span>') # red arrow up
+        body = body.replace('&#x2198','<span style="color: green"> &#x2198;</span>') # green arrow down
+        content = top + toti + body + bottom
+        out.write(content)
+except Exception as e:
+    print(f'Error:\n{e}')
