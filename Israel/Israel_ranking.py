ce6eefa6b32234296d03521700b98a852d713a78
Jason Li <aochen.jli@gmail.com>
2021-04-09 18:40:17 -0700

    Update Israel_ranking.py

--- Israel/Israel_ranking.py
+++ Israel/Israel_ranking.py
@@ -1,28 +1,30 @@
 import numpy as np
-from io import BytesIO
-from zipfile import ZipFile
-from urllib.request import urlopen
 import datetime
 from datetime import timedelta
 import pandas as pd
 
-
-url1 = f"https://chromedriver.storage.googleapis.com/90.0.4430.24/chromedriver_linux64.zip"
-resp = urlopen(url1)
-with ZipFile(BytesIO(resp.read()), 'r') as zipObj:
-   zipObj.extractall()
-mode = os.stat('./chromedriver').st_mode
-os.chmod('./chromedriver', mode | 0o111)
-
 import os
+from io import BytesIO
+from urllib.request import urlopen
+from zipfile import ZipFile
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+
+chromedriver_url = "https://chromedriver.storage.googleapis.com/89.0.4389.23/chromedriver_mac64.zip"
+resp = urlopen(chromedriver_url)
+with ZipFile(BytesIO(resp.read()), 'r') as zipObj:
+    zipObj.extractall()
+mode = os.stat('./chromedriver').st_mode
+os.chmod('./chromedriver', mode | 0o111)
+
 options = webdriver.ChromeOptions()
 options.add_argument("--disable-blink-features")
 options.add_argument("--disable-blink-features=AutomationControlled")
-driver = webdriver.Chrome(options=options, executable_path='./chromedriver')
+driver = webdriver.Chrome(options=options, executable_path=r'./chromedriver')
+
 url = "https://data.gov.il/dataset/covid-19"
 driver.get(url)
 WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "content")))
