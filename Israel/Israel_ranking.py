4f3a4a7c04e0bf120c2c3ee8d7bdb62278a777de
Jason Li <aochen.jli@gmail.com>
2021-04-09 19:20:29 -0700

    Update Israel_ranking.py

--- Israel/Israel_ranking.py
+++ Israel/Israel_ranking.py
@@ -22,6 +22,7 @@ os.chmod('./chromedriver', mode | 0o111)
 
 options = webdriver.ChromeOptions()
 options.add_argument("--no-sandbox")
+options.add_argument("--headless")
 options.add_argument("--disable-dev-shm-usage")
 options.add_argument("--disable-blink-features")
 options.add_argument("--disable-blink-features=AutomationControlled")
