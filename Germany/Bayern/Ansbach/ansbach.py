fa2df9cda592e61ca9889f165b7864fb0b5a3c61
vbrunsch <62330755+vbrunsch@users.noreply.github.com>
2021-05-10 19:48:38 -0400

    Update ansbach.py

--- Germany/Bayern/Ansbach/ansbach.py
+++ Germany/Bayern/Ansbach/ansbach.py
@@ -16,6 +16,8 @@ neu = pd.DataFrame()
 
 tod = pd.Timestamp.today()
 tod = tod.strftime('%d.%m.%Y')
+yes = pd.Timestamp.today() - timedelta(days = 1)
+yes = yes.strftime('%d.%m.%Y')
 
 url = 'https://www.landkreis-ansbach.de/Corona'
 html = urllib.request.urlopen(url)
@@ -23,10 +25,15 @@ htmlParse = BeautifulSoup(html, 'html.parser')
 lin = re.findall('class="csslink_PDF" href="(.*)" target="_blank">', str(htmlParse))
 
 lind = re.findall('class="csslink_PDF" href="(.*)', str(htmlParse))
-day = re.findall('Stand .?(.)\.',lind[0])
+day = re.findall('Stand .?(.)\.',lind[1])
 
-if day[0] == tod[1]:
-    pdf_path = 'https://www.landkreis-ansbach.de' + lin[0]
+print('Day:')
+print(day)
+print('Today:')
+print(tod)
+
+if day[0] == yes[1]:
+    pdf_path = 'https://www.landkreis-ansbach.de' + lin[1]
     dfs = tabula.read_pdf(pdf_path, stream=True)
     df = dfs[0][:-2]
     neu = df.copy()
