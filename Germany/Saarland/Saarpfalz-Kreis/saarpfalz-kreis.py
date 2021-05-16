f5f3604526dfe4c1db5a381e557d9990f22354ce
vbrunsch <62330755+vbrunsch@users.noreply.github.com>
2021-05-15 20:27:06 -0400

    Update saarpfalz-kreis.py

--- Germany/Saarland/Saarpfalz-Kreis/saarpfalz-kreis.py
+++ Germany/Saarland/Saarpfalz-Kreis/saarpfalz-kreis.py
@@ -8,7 +8,7 @@ df = df.set_index(df.columns[0])
 df.columns = df.iloc[0]
 df = df[1:]
 df.index.name = None
-to = pd.Timestamp.today() - timedelta(days=1)
+to = pd.Timestamp.today()# - timedelta(days=1)
 tod = to.strftime('%m_%d_%Y')
 if tod == '05_13_2021':
     df['Veränderung zum Vortag'] = 0
