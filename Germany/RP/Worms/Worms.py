0594ee5fe3fabf3a4ecdc17bb17620a5f13e3071
vbrunsch <62330755+vbrunsch@users.noreply.github.com>
2021-05-31 20:15:44 -0400

    Update Worms.py

--- Germany/RP/Worms/Worms.py
+++ Germany/RP/Worms/Worms.py
@@ -22,12 +22,12 @@ kon = pd.DataFrame()
 zus = pd.DataFrame()
 we = 0
 
-for x in range(0,14):
+for x in range(1,15):
     x = x + we*2
     m = 0
     tod = pd.Timestamp.today() -timedelta(days=x)
     tod = tod.strftime('%d.%m.%Y')
-    while m == 0 and x < 14:
+    while m == 0 and x < 15:
         for url in ['https://www.kreis-alzey-worms.eu/verwaltung/aktuelles/','https://www.kreis-alzey-worms.eu/verwaltung/aktuelles/?pageId586a9b2b=2#list_586a9b2b','https://www.kreis-alzey-worms.eu/verwaltung/aktuelles/?pageId586a9b2b=3#list_586a9b2b','https://www.kreis-alzey-worms.eu/verwaltung/aktuelles/?pageId586a9b2b=4#list_586a9b2b','https://www.kreis-alzey-worms.eu/verwaltung/aktuelles/?pageId586a9b2b=5#list_586a9b2b','https://www.kreis-alzey-worms.eu/verwaltung/aktuelles/?pageId586a9b2b=6#list_586a9b2b']:
             if tod == '27.04.2021':
               m = m + 1
@@ -44,7 +44,7 @@ for x in range(0,14):
                 #print(t)
                 matches = re.findall(tod, t)
                 if matches:
-                    if re.findall('Corona\-?: Heute',t) or re.findall('Corona: Seit',t):
+                    if re.findall('Corona\-?: Heute',t) or re.findall('Corona\-?: heute',t) or re.findall('Corona: Seit',t):
                         m = m + 1
                         print(matches[0])
                         lin = re.findall('href="(.*)">Corona', str(para))
