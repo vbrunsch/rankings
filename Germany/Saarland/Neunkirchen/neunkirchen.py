bf9105afccfba3f62d26e5de4184e8f67b9530b6
vbrunsch <62330755+vbrunsch@users.noreply.github.com>
2021-06-01 21:11:33 -0400

    Update neunkirchen.py

--- Germany/Saarland/Neunkirchen/neunkirchen.py
+++ Germany/Saarland/Neunkirchen/neunkirchen.py
@@ -19,7 +19,7 @@ df = pd.DataFrame(index = ['Neunkirchen','Illingen','Ottweiler','Eppelborn','Mer
 import re
 
 from datetime import timedelta
-for x in range(0,14):
+for x in range(1,15):
     tod = pd.Timestamp.today() -timedelta(days=x)
     tod = tod.strftime('%d.%m.%Y')
     for para in htmlParse.find_all("p"): 
@@ -37,6 +37,8 @@ for x in range(0,14):
                 df[tod] = [int(ne),int(il),int(ot),int(ep),int(me),int(sc),int(sp)]
             else:
                 df[tod] = [0,0,0,0,0,0,0]
+    if tod not in df.columns:
+      df[tod] = [0,0,0,0,0,0,0]
 print(df)
 
 import numpy as np
