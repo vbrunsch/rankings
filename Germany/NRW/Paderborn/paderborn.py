2778484b6f482f893c80dfdce5f8711b59bc3864
vbrunsch <62330755+vbrunsch@users.noreply.github.com>
2021-06-09 23:07:22 -0400

    Create paderborn.py

---
+++ Germany/NRW/Paderborn/paderborn.py
@@ -0,0 +1,206 @@
+import bs4
+from bs4 import BeautifulSoup
+import pandas as pd
+import time 
+import requests
+import re
+from datetime import timedelta
+
+da = pd.Timestamp.today() - timedelta(days = 17)
+das = da.strftime('%Y-%m-%d')
+
+que = f'https://utility.arcgis.com/usrsvcs/servers/9020e4c3f15b40a6807cf282504e26f2/rest/services/secure/KPB_CoronaDashboard_Prod_Secure/MapServer/1/query?f=json&where=(AKT_STAND%20BETWEEN%20timestamp%20%27{das}%2004%3A00%3A00%27%20AND%20CURRENT_TIMESTAMP)&returnGeometry=false&spatialRel=esriSpatialRelIntersects&outFields=*&orderByFields=AKT_STAND%20asc&resultOffset=0&resultRecordCount=10000'
+
+t3 = requests.get(que).text
+
+to = pd.Timestamp.today() - timedelta(days = 1)
+tod = to.strftime('%m_%d_%Y')
+
+gem = re.findall('GEMEINDE":"(.*?)"',t3)
+inf = re.findall('BE_AKTUELL":(.*?),',t3)
+infy = re.findall('ZI_AKTUELL":(.*?),',t3)
+dat = re.findall('AKT_STAND":(.*?),',t3)
+
+df = pd.DataFrame(data = inf, index = gem)
+df['neu'] = infy
+df['Datum'] = dat
+df.columns = df.iloc[0]
+df = df[1:]
+df['Datum'] = df['"Stand der Aktualität"'].astype(int)
+import datetime
+df['Datum'] = df['Datum']/1000
+df['Datum'] = df['Datum'].apply(lambda x: datetime.datetime.fromtimestamp(x))
+df['Datum'] = df['Datum'].apply(lambda x: x.strftime('%m_%d_%Y'))
+df = df.drop(['"Stand der Aktualität"'], axis =1)
+print(df)
+df.to_csv(f'Germany/NRW/Paderborn/data/Paderborn_{tod}.csv')
+
+hdf = df.copy()
+l7 = []
+l14 = []
+
+for i in hdf.index.unique():
+  idf = df.loc[[i]].copy()
+  idf['neu'] = idf['"Bestätigte Fälle"'].astype(int).diff()
+  l7.append(sum(idf.loc[[i]][-7:]['neu'].astype(int)))
+  l14.append(sum(idf.loc[[i]][-14:]['neu'].astype(int)))
+
+neg_l7=[0 if i>=0 else i for i in l7]
+l7 = [l7[i]-neg_l7[i] for i in range(len(l7))]
+l14 = [l14[i]+neg_l7[i] if i>0 else 0 for i in range(len(l14))]
+l14 = [i if i>= 0 else 0 for i in l14]
+
+zus = pd.DataFrame(index = hdf.index.unique(), data = l7, columns = ['last7'])
+zus['last14'] = l14
+
+import numpy as np
+zus['mix'] = np.where(zus['last7'] == 0, 0.6, zus['last7'])
+zus['mix'] = np.where(zus['last14'] == 0, 0.2, zus['mix'])
+zus['Gemeinde'] = zus.index
+zus = zus.drop('(nicht zuordnebar)')
+zus = zus.drop('(gesamter Kreis)')
+print(zus)
+
+zus.to_csv(f'Germany/NRW/Paderborn/data/Paderborn_for_dw14_7.csv') 
+
+# For Rankings
+mdf = zus.copy()
+mdf = mdf.drop_duplicates()
+mdf['Neuzugänge letzten 7 Tage_x'] = mdf['last7']
+mdf['Neuzugänge letzten 14 Tage'] = mdf['last14']
+mdf['Neuzugänge letzten 7 Tage_y'] = mdf['Neuzugänge letzten 14 Tage']-mdf['Neuzugänge letzten 7 Tage_x']  
+mdf['Covid-freie Wochen'] = 0
+mdf['Covid-freie Wochen'] = np.where(mdf['Neuzugänge letzten 7 Tage_x'] == 0, 1, mdf['Covid-freie Wochen'])
+mdf['Covid-freie Wochen'] = np.where(mdf['Neuzugänge letzten 14 Tage'] == 0 , 2, mdf['Covid-freie Wochen'])
+mdf = mdf[['Gemeinde','Covid-freie Wochen','Neuzugänge letzten 14 Tage','Neuzugänge letzten 7 Tage_x','Neuzugänge letzten 7 Tage_y']]
+#print(mdf)
+
+cols=['Gemeinde','Covid-freie Wochen','Neuzugänge letzten 14 Tage','Neuzugänge letzten 7 Tage_x','Neuzugänge letzten 7 Tage_y']
+thr = pd.DataFrame(mdf, columns=cols)
+fin = thr.sort_values(['Covid-freie Wochen'], ascending=[False])
+fin['week'] = fin['Covid-freie Wochen'].gt(13) 
+tab = fin.sort_values(['week'], ascending=[False])
+tab_t = tab[tab['week']==True]
+tab_f = tab[tab['week']==False]
+tab_f = tab_f.sort_values(['Neuzugänge letzten 7 Tage_x','Neuzugänge letzten 14 Tage'], ascending = [True,True])
+tab_t = tab_t.sort_values(['Covid-freie Wochen','Neuzugänge letzten 7 Tage_x'], ascending = [False,True])
+tab = tab_t.append(tab_f)
+tab = tab.drop(['week'], axis=1)
+
+#Percent Change
+
+tab['PercentChange'] = 100*(tab['Neuzugänge letzten 7 Tage_x'] - tab['Neuzugänge letzten 7 Tage_y'])/(tab['Neuzugänge letzten 7 Tage_x']+tab['Neuzugänge letzten 7 Tage_y'])
+tab['PercentChange'] = tab['PercentChange'].fillna(0.0)
+
+tab = tab.drop(['Neuzugänge letzten 7 Tage_y'], axis = 1)
+#tab.columns = ['Gemeinde', 'Covid-freie Wochen', 'Neue Fälle letzte 14 Tage', 'Letzte 7 Tage', 'Pct Change']
+
+def highlighter(s):
+    val_1 = s['Letzte 7 Tage']
+    val_2 = s['Neue Fälle letzte 14 Tage']
+    
+    r=''
+    try:
+        if 0==val_2: #More than 2 Covid free weeks
+            r = 'background-color: #24773B; color: #ffffff;'
+        elif 0==val_1 : # less than 20 in last 2 weeks
+            r = 'background-color: #89c540;' 
+        elif 10>=val_1 >=1: #Yellow
+            r = 'background-color: #f9cc3d;'
+        elif 35>=val_1 >= 11: #Orange
+            r = 'background-color: #f8961d;'
+        elif 100>=val_1 >= 36: #Light Red
+            r = 'background-color: #ef3d23;'
+        elif 1000>=val_1 > 100: # Red
+            r = 'background-color: #B11F24;'
+        elif 10000>=val_1 > 1000: # Light Purple
+            r = 'background-color: #652369;'
+        elif val_1 > 10000: # Purple
+            r = 'background-color: #36124B; color: #ffffff;'
+    except Exception as e:
+        r = 'background-color: white'
+    if 0 == val_2:
+        return [r]*(len(s)-2) + ['']*2
+    else:
+        return [r]*(len(s)-3) + [''] + [r] + ['']
+
+def hover(hover_color="#ffff99"):
+    return dict(selector="tbody tr:hover td, tbody tr:hover th",
+                props=[("background-color", "rgba(66, 165, 245, 0.2) !important")])
+
+top = """
+<!DOCTYPE html>
+<meta content="text/html;charset=utf-8" http-equiv="Content-Type">
+<meta content="utf-8" http-equiv="encoding">
+<html>
+<head>
+<style>
+    h2 {
+        text-align: center;
+        font-family: Helvetica, Arial, sans-serif;
+    }
+    table { 
+        margin-left: auto;
+        margin-right: auto;
+    }
+    table, th, td {
+        border: 1px solid black;
+        border-collapse: collapse;
+    }
+    th, td {
+        padding: 5px;
+        text-align: center;
+        font-family: Helvetica, Arial, sans-serif;
+        font-size: 90%;
+    }
+    table tbody tr:hover {
+        background-color: #dddddd;
+    }
+    /*
+    table tbody tr:hover td, table tbody tr:hover th {
+  background-color: #dddddd !important;
+    }
+    /*
+    .wide {
+        width: 90%; 
+    }
+</style>
+</head>
+<body>
+"""
+bottom = """
+</body>
+</html>
+"""
+
+arrow = lambda x : ' &#x2197;' if x>0 else (' &#x2192' if x ==0  else ' &#x2198')
+styles=[hover(),]
+tab['Platz'] = tab.reset_index().index
+tab['Platz'] = tab['Platz'].add(1)
+tab['Platz'] = np.where(tab['Neuzugänge letzten 14 Tage'] == 0 , 1, tab['Platz'])
+tab['Trend'] = tab['PercentChange'].map(arrow)
+tab['Percent Change'] = tab['PercentChange'].map('{:,.2f}%'.format) + tab['Trend']
+tab = tab.drop(['Trend','PercentChange'], axis = 1)
+
+tab.rename(columns = {'Neuzugänge letzten 14 Tage':'Neue Fälle letzte 14 Tage'}, inplace = True)
+tab.rename(columns = {'Neuzugänge letzten 7 Tage_x':'Letzte 7 Tage'}, inplace = True)
+tab.rename(columns = {'Percent Change':'Trend'}, inplace = True)
+tab.rename(columns = {'Gemeinde':'Stadt/Gemeinde'}, inplace = True)
+
+
+tab = tab[['Platz', 'Stadt/Gemeinde', 'Covid-freie Wochen', 'Neue Fälle letzte 14 Tage', 'Letzte 7 Tage','Trend']]
+tab = tab.drop('Covid-freie Wochen', axis = 1)
+s = tab.style.apply(highlighter, axis = 1).set_table_styles(styles).hide_index()
+
+import time
+toti = time.strftime('%m/%d/%Y %H:%M:%S')
+toti = "<center><caption>Last Update: " + toti + " UTC</caption></center>"
+
+try:        
+    with open(f'Paderborn.html', 'w', encoding="utf-8") as out:
+        body = s.render().replace('&#x2197;','<span style="color: red"> &#x2197;</span>') # red arrow up
+        body = body.replace('&#x2198','<span style="color: green"> &#x2198;</span>') # green arrow down
+        content = top + toti + body + bottom
+        out.write(content)
+except Exception as e:
+    print(f'Error:\n{e}')
