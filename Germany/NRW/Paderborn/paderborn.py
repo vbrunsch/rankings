eee0874b68a07b69012fc6e4c81bf33e1053c347
vbrunsch <62330755+vbrunsch@users.noreply.github.com>
2021-06-11 23:52:56 -0400

    Update paderborn.py

--- Germany/NRW/Paderborn/paderborn.py
+++ Germany/NRW/Paderborn/paderborn.py
@@ -54,6 +54,12 @@ zus = pd.DataFrame(index = hdf.index.unique(), data = l7, columns = ['last7'])
 zus['last14'] = l14
 
 import numpy as np
+zus['neg_l7']=np.where(zus['last7']< 0, zus['last7'], 0)
+zus['last7']= zus['last7']-zus['neg_l7']
+zus['last14']=np.where(zus['last14']< 0, zus['last14']-zus['neg_l7'], zus['last14']+zus['neg_l7'])
+zus['last14']=np.where(zus['last14']< zus['last7'], zus['last7'], zus['last14'])
+zus = zus[['last7','last14']]
+
 zus['mix'] = np.where(zus['last7'] == 0, 0.6, zus['last7'])
 zus['mix'] = np.where(zus['last14'] == 0, 0.2, zus['mix'])
 zus['Gemeinde'] = zus.index
