8ea635b8c06112f0fb418596bdad47f75dfcb741
vbrunsch <62330755+vbrunsch@users.noreply.github.com>
2021-06-13 00:33:49 -0400

    Update rsk.py

--- Germany/NRW/RSK/rsk.py
+++ Germany/NRW/RSK/rsk.py
@@ -11,7 +11,7 @@ t3 = requests.get(que).text
 
 
 from datetime import timedelta
-to = pd.Timestamp.today() - timedelta(days = 1)
+to = pd.Timestamp.today()# - timedelta(days = 1)
 tod = to.strftime('%m_%d_%Y')
 
 gem = re.findall('NAME":"(.*?)"',t3)
