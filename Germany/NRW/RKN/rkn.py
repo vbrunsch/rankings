d5aa66c076f3d38eba37b17643b9483daed68acb
vbrunsch <62330755+vbrunsch@users.noreply.github.com>
2021-06-15 00:11:13 -0400

    Create rkn.py

---
+++ Germany/NRW/RKN/rkn.py
@@ -0,0 +1,199 @@
+import bs4
+from bs4 import BeautifulSoup
+import pandas as pd
+import time 
+import requests
+import re
+
+que = 'https://www.status-neuss.de/api/dashboard/data/DashboardItemGetAction?dashboardId=VerteilungPostleitzahlen&itemId=gridDashboardItem1'
+
+t3 = requests.get(que).text
+
+from datetime import timedelta
+to = pd.Timestamp.today() - timedelta(days = 1)
+tod = to.strftime('%m_%d_%Y')
+
+gem = ["40667 Meerbusch","40668 Meerbusch","40670 Meerbusch","41352 Korschenbroich","41363 Jüchen","41460 Neuss","41462 Neuss","41464 Neuss","41466 Neuss","41468 Neuss","41469 Neuss","41470 Neuss","41472 Neuss","41515 Grevenbroich","41516 Grevenbroich","41517 Grevenbroich","41539 Dormagen","41540 Dormagen","41541 Dormagen","41542 Dormagen","41564 Kaarst","41569 Rommerskirchen"]
+inf = re.findall('"7":(.*?),',t3)
+
+df = pd.DataFrame(data = inf, index = gem)
+df.columns = [tod]
+df = df.astype(int)
+print(df)
+df.to_csv(f'Rhein-Kreis-Neuss_PLZ_{tod}.csv')
+
+ge = ['Meerbusch','Korschenbroich','Jüchen','Neuss','Grevenbroich','Dormagen','Kaarst','Rommerskirchen']
+ges = [sum(df[df.columns[0]][0:3]),df[df.columns[0]][3],df[df.columns[0]][4],sum(df[df.columns[0]][5:13]),sum(df[df.columns[0]][13:16]),sum(df[df.columns[0]][16:20]),df[df.columns[0]][20],df[df.columns[0]][21]]
+df2 = pd.DataFrame(data = ges, index = ge)
+df2.columns = [tod]
+print(df2)
+df2.to_csv(f'Germany/NRW/RKN/data/Rhein-Kreis-Neuss_{tod}.csv')
+
+import numpy as np
+
+da7 = to - timedelta(days = 7)
+da7s = da7.strftime('%m_%d_%Y')
+da14 = to - timedelta(days = 14)
+da14s = da14.strftime('%m_%d_%Y')
+old7 = pd.read_csv(f'Germany/NRW/RKN/data/Rhein-Kreis-Neuss_{da7s}.csv', index_col = 0)
+old14 = pd.read_csv(f'Germany/NRW/RKN/data/Rhein-Kreis-Neuss_{da14s}.csv', index_col = 0)
+
+zus = df2.copy()
+zus['last7'] = zus[zus.columns[0]].astype(int) - old7[old7.columns[0]].astype(int)
+zus['last14'] = zus[zus.columns[0]].astype(int) - old14[old14.columns[0]].astype(int)
+
+zus['neg_l7']=np.where(zus['last7']< 0, zus['last7'], 0)
+zus['last7']= zus['last7']-zus['neg_l7']
+zus['last14']=np.where(zus['last14']< 0, zus['last14']-zus['neg_l7'], zus['last14']+zus['neg_l7'])
+zus['last14']=np.where(zus['last14']< zus['last7'], zus['last7'], zus['last14'])
+
+zus = zus[['last7','last14']]
+zus['mix'] = np.where(zus['last7'] == 0, 0.6, zus['last7'])
+zus['mix'] = np.where(zus['last14'] == 0, 0.2, zus['mix'])
+zus['Gemeinde'] = zus.index
+
+print(zus)
+zus.to_csv(f'Germany/NRW/RKN/data/Rhein-Kreis-Neuss_for_dw14_7.csv') 
+
+
+# For Rankings
+mdf = zus.copy()
+mdf = mdf.drop_duplicates()
+mdf['Neuzugänge letzten 7 Tage_x'] = mdf['last7']
+mdf['Neuzugänge letzten 14 Tage'] = mdf['last14']
+mdf['Neuzugänge letzten 7 Tage_y'] = mdf['Neuzugänge letzten 14 Tage']-mdf['Neuzugänge letzten 7 Tage_x']  
+mdf['Covid-freie Wochen'] = 0
+mdf['Covid-freie Wochen'] = np.where(mdf['Neuzugänge letzten 7 Tage_x'] == 0, 1, mdf['Covid-freie Wochen'])
+mdf['Covid-freie Wochen'] = np.where(mdf['Neuzugänge letzten 14 Tage'] == 0 , 2, mdf['Covid-freie Wochen'])
+mdf = mdf[['Gemeinde','Covid-freie Wochen','Neuzugänge letzten 14 Tage','Neuzugänge letzten 7 Tage_x','Neuzugänge letzten 7 Tage_y']]
+#print(mdf)
+
+cols=['Gemeinde','Covid-freie Wochen','Neuzugänge letzten 14 Tage','Neuzugänge letzten 7 Tage_x','Neuzugänge letzten 7 Tage_y']
+thr = pd.DataFrame(mdf, columns=cols)
+fin = thr.sort_values(['Covid-freie Wochen'], ascending=[False])
+fin['week'] = fin['Covid-freie Wochen'].gt(13) 
+tab = fin.sort_values(['week'], ascending=[False])
+tab_t = tab[tab['week']==True]
+tab_f = tab[tab['week']==False]
+tab_f = tab_f.sort_values(['Neuzugänge letzten 7 Tage_x','Neuzugänge letzten 14 Tage'], ascending = [True,True])
+tab_t = tab_t.sort_values(['Covid-freie Wochen','Neuzugänge letzten 7 Tage_x'], ascending = [False,True])
+tab = tab_t.append(tab_f)
+tab = tab.drop(['week'], axis=1)
+
+#Percent Change
+
+tab['PercentChange'] = 100*(tab['Neuzugänge letzten 7 Tage_x'] - tab['Neuzugänge letzten 7 Tage_y'])/(tab['Neuzugänge letzten 7 Tage_x']+tab['Neuzugänge letzten 7 Tage_y'])
+tab['PercentChange'] = tab['PercentChange'].fillna(0.0)
+
+tab = tab.drop(['Neuzugänge letzten 7 Tage_y'], axis = 1)
+#tab.columns = ['Gemeinde', 'Covid-freie Wochen', 'Neue Fälle letzte 14 Tage', 'Letzte 7 Tage', 'Pct Change']
+
+def highlighter(s):
+    val_1 = s['Letzte 7 Tage']
+    val_2 = s['Neue Fälle letzte 14 Tage']
+    
+    r=''
+    try:
+        if 0==val_2: #More than 2 Covid free weeks
+            r = 'background-color: #24773B; color: #ffffff;'
+        elif 0==val_1 : # less than 20 in last 2 weeks
+            r = 'background-color: #89c540;' 
+        elif 10>=val_1 >=1: #Yellow
+            r = 'background-color: #f9cc3d;'
+        elif 35>=val_1 >= 11: #Orange
+            r = 'background-color: #f8961d;'
+        elif 100>=val_1 >= 36: #Light Red
+            r = 'background-color: #ef3d23;'
+        elif 1000>=val_1 > 100: # Red
+            r = 'background-color: #B11F24;'
+        elif 10000>=val_1 > 1000: # Light Purple
+            r = 'background-color: #652369;'
+        elif val_1 > 10000: # Purple
+            r = 'background-color: #36124B; color: #ffffff;'
+    except Exception as e:
+        r = 'background-color: white'
+    if 0 == val_2:
+        return [r]*(len(s)-2) + ['']*2
+    else:
+        return [r]*(len(s)-3) + [''] + [r] + ['']
+
+def hover(hover_color="#ffff99"):
+    return dict(selector="tbody tr:hover td, tbody tr:hover th",
+                props=[("background-color", "rgba(66, 165, 245, 0.2) !important")])
+
+top = """
+<!DOCTYPE html>
+<meta content="text/html;charset=utf-8" http-equiv="Content-Type">
+<meta content="utf-8" http-equiv="encoding">
+<html>
+<head>
+<style>
+    h2 {
+        text-align: center;
+        font-family: Helvetica, Arial, sans-serif;
+    }
+    table { 
+        margin-left: auto;
+        margin-right: auto;
+    }
+    table, th, td {
+        border: 1px solid black;
+        border-collapse: collapse;
+    }
+    th, td {
+        padding: 5px;
+        text-align: center;
+        font-family: Helvetica, Arial, sans-serif;
+        font-size: 90%;
+    }
+    table tbody tr:hover {
+        background-color: #dddddd;
+    }
+    /*
+    table tbody tr:hover td, table tbody tr:hover th {
+  background-color: #dddddd !important;
+    }
+    /*
+    .wide {
+        width: 90%; 
+    }
+</style>
+</head>
+<body>
+"""
+bottom = """
+</body>
+</html>
+"""
+
+arrow = lambda x : ' &#x2197;' if x>0 else (' &#x2192' if x ==0  else ' &#x2198')
+styles=[hover(),]
+tab['Platz'] = tab.reset_index().index
+tab['Platz'] = tab['Platz'].add(1)
+tab['Platz'] = np.where(tab['Neuzugänge letzten 14 Tage'] == 0 , 1, tab['Platz'])
+tab['Trend'] = tab['PercentChange'].map(arrow)
+tab['Percent Change'] = tab['PercentChange'].map('{:,.2f}%'.format) + tab['Trend']
+tab = tab.drop(['Trend','PercentChange'], axis = 1)
+
+tab.rename(columns = {'Neuzugänge letzten 14 Tage':'Neue Fälle letzte 14 Tage'}, inplace = True)
+tab.rename(columns = {'Neuzugänge letzten 7 Tage_x':'Letzte 7 Tage'}, inplace = True)
+tab.rename(columns = {'Percent Change':'Trend'}, inplace = True)
+tab.rename(columns = {'Gemeinde':'Stadt/Gemeinde'}, inplace = True)
+
+
+tab = tab[['Platz', 'Stadt/Gemeinde', 'Covid-freie Wochen', 'Neue Fälle letzte 14 Tage', 'Letzte 7 Tage','Trend']]
+tab = tab.drop('Covid-freie Wochen', axis = 1)
+s = tab.style.apply(highlighter, axis = 1).set_table_styles(styles).hide_index()
+
+import time
+toti = time.strftime('%m/%d/%Y %H:%M:%S')
+toti = "<center><caption>Last Update: " + toti + " UTC</caption></center>"
+
+try:        
+    with open(f'Rhein-Kreis-Neuss.html', 'w', encoding="utf-8") as out:
+        body = s.render().replace('&#x2197;','<span style="color: red"> &#x2197;</span>') # red arrow up
+        body = body.replace('&#x2198','<span style="color: green"> &#x2198;</span>') # green arrow down
+        content = top + toti + body + bottom
+        out.write(content)
+except Exception as e:
+    print(f'Error:\n{e}')
