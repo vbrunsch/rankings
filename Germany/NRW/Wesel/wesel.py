d8e7039aa77d9c4bd626c01cc3987b4b3d354db5
vbrunsch <62330755+vbrunsch@users.noreply.github.com>
2021-07-05 23:07:24 -0400

    Update wesel.py

--- Germany/NRW/Wesel/wesel.py
+++ Germany/NRW/Wesel/wesel.py
@@ -12,7 +12,7 @@ import requests
 import numpy as np
 
 
-to = pd.Timestamp.today()- timedelta(days = 1)
+to = pd.Timestamp.today()#- timedelta(days = 1)
 tod = to.strftime('%m_%d_%Y')
 
 url_s = 'https://www.kreis-wesel.de/de/themen/corona-fallzahlen/'
@@ -43,8 +43,8 @@ except:
 df = df[['Gesamtfallzahlen']]
 df = df.astype(int)
 print(df)
-#df.to_csv(f'Germany/NRW/Wesel/data/Wesel_{tod}.csv')
-df = pd.read_csv(f'Germany/NRW/Wesel/data/Wesel_{tod}.csv', index_col = 0)
+df.to_csv(f'Germany/NRW/Wesel/data/Wesel_{tod}.csv')
+#df = pd.read_csv(f'Germany/NRW/Wesel/data/Wesel_{tod}.csv', index_col = 0)
 
 da7 = to - timedelta(days = 7)
 da7s = da7.strftime('%m_%d_%Y')
