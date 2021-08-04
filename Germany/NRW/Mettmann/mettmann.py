0d727c6e45742b4d02083f094e2683706857a8ff
vbrunsch <62330755+vbrunsch@users.noreply.github.com>
2021-08-03 21:58:26 -0400

    Update mettmann.py

--- Germany/NRW/Mettmann/mettmann.py
+++ Germany/NRW/Mettmann/mettmann.py
@@ -33,16 +33,15 @@ url2 = url2.replace('amp;','')
 #url2 = 'https://www.kreis-mettmann-corona.de/Aktuelle-Meldungen/Corona-Virus-130-Infizierte-23-510-Genesene-Inzidenz-18-7.php?object=tx,3535.5.1&ModID=7&FID=3535.324.1&NavID=3535.12&La=1&kat=3535.3'
 html2 = urllib.request.urlopen(url2)
 htmlParse2 = BeautifulSoup(html2, 'html.parser')
-lin2 = re.findall('"(\/media.*?g\.PNG)"', str(htmlParse2))
-imp = re.findall('"\/media\/custom\/(.*?g\.PNG)"', str(htmlParse2))
+lin2 = re.findall('"(\/media.*?g\.PNG.*)"', str(htmlParse2))
+imp = re.findall('"\/media\/custom\/(.*?g\.PNG.*?)"', str(htmlParse2))
 print(lin2)
-im_p = '"' + 'https://www.kreis-mettmann-corona.de' + lin2[0] + '"'
-
+im_p = '"' + 'https://www.kreis-mettmann-corona.de' + lin2[1] + '"'
 
 os.system(f"wget {im_p}")
 #$wget ${im_p}
 
-ext_i = pytesseract.image_to_string(Image.open(imp[0]))
+ext_i = pytesseract.image_to_string(Image.open(imp[1]))
 
 
 infa = re.findall('[^\/].*?\/.*?\/([0-9]*)[^\/0-9]',ext_i)
@@ -54,7 +53,7 @@ inf = infnp[infnp>1000]
 gem = ['Erkrath','Hilden','Monheim','Wülfrath','Haan','Langenfeld','Ratingen','Kreis Mettmann','Heiligenhaus','Mettmann','Velbert']
 df = pd.DataFrame(data = inf, index = gem)
 df.columns = ['Gesamtfallzahlen']
-to = pd.Timestamp.today()# - timedelta(days = 1)
+to = pd.Timestamp.today() - timedelta(days = 1)
 tod = to.strftime('%m_%d_%Y')
 print(df)
 df.to_csv(f'Germany/NRW/Mettmann/data/Mettmann_{tod}.csv')
