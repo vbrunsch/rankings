567c20e7c8da48d684a93f31ef1f082c5607915d
vbrunsch <62330755+vbrunsch@users.noreply.github.com>
2021-08-25 23:48:09 -0400

    Update viersen.py

--- Germany/NRW/Viersen/viersen.py
+++ Germany/NRW/Viersen/viersen.py
@@ -19,7 +19,7 @@ for i in links:
   t4 = re.findall('Meldungsdatum: (.*?)<',t3)
   t5 = re.findall('(.*?) verteilen sich wie folgt auf die ',t3)
   from datetime import timedelta
-  to = pd.Timestamp.today() - timedelta(days = 1)
+  to = pd.Timestamp.today()# - timedelta(days = 1)
   tod = to.strftime('%m_%d_%Y')
   tos = to.strftime('%d.%m.%Y')
   if tos == t4[0] and t5:
