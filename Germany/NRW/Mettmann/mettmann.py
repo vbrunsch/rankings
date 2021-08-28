331308132b8cdf7b692844009ead1d2f2182af83
vbrunsch <62330755+vbrunsch@users.noreply.github.com>
2021-08-27 21:27:40 -0400

    Update mettmann.py

--- Germany/NRW/Mettmann/mettmann.py
+++ Germany/NRW/Mettmann/mettmann.py
@@ -33,7 +33,7 @@ url2 = url2.replace('amp;','')
 #url2 = 'https://www.kreis-mettmann-corona.de/Aktuelle-Meldungen/Corona-Virus-130-Infizierte-23-510-Genesene-Inzidenz-18-7.php?object=tx,3535.5.1&ModID=7&FID=3535.324.1&NavID=3535.12&La=1&kat=3535.3'
 html2 = urllib.request.urlopen(url2)
 htmlParse2 = BeautifulSoup(html2, 'html.parser')
-lin2 = re.findall('"(\/media.*?g\.PNG.*)"', str(htmlParse2))
+lin2 = re.findall('"(\/media.*?g\.JPG.*)"', str(htmlParse2))
 imp = re.findall('" href="\/media\/custom\/(.*?g\.JPG.*?)"', str(htmlParse2))
 print(lin2)
 im_p = '"' + 'https://www.kreis-mettmann-corona.de' + lin2[1] + '"'
