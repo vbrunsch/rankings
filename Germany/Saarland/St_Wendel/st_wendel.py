6cb8d0be2680c632b631ebb77b4736c73aade390
vbrunsch <62330755+vbrunsch@users.noreply.github.com>
2021-09-04 20:51:43 -0400

    Update st_wendel.py

--- Germany/Saarland/St_Wendel/st_wendel.py
+++ Germany/Saarland/St_Wendel/st_wendel.py
@@ -9,7 +9,7 @@ try:
     df = df.set_index(df.columns[0])
     df = df[:-2]
     df.index.name = None
-    to = pd.Timestamp.today() - timedelta(days=1)
+    to = pd.Timestamp.today()# - timedelta(days=1)
     tod = to.strftime('%m_%d_%Y')
     tods = to.strftime('%d.%m.%Y')
     df.to_csv(f'Germany/Saarland/St_Wendel/data/St_Wendel_{tod}.csv')
