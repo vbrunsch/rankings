42d98ba474e7868acf287496382a0ad36eeab613
vbrunsch <62330755+vbrunsch@users.noreply.github.com>
2021-11-03 20:56:27 -0400

    Update Israel_ranking.py

--- Israel/Israel_ranking.py
+++ Israel/Israel_ranking.py
@@ -14,9 +14,10 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+#96.0.4664.18
 #93.0.4577.15
 #91.0.4472.19
-chromedriver_url = "https://chromedriver.storage.googleapis.com/93.0.4577.15/chromedriver_linux64.zip"
+chromedriver_url = "https://chromedriver.storage.googleapis.com/96.0.4664.18/chromedriver_linux64.zip"
 resp = urlopen(chromedriver_url)
 with ZipFile(BytesIO(resp.read()), 'r') as zipObj:
     zipObj.extractall()
@@ -224,7 +225,7 @@ if not df_new.equals(df_old):
     tab = tab[['Rank', 'Area', 'COVID-Free Days', 'New Cases in Last 14 Days','Last 7 Days','Percent Change']]       
     s = tab.style.apply(highlighter, axis = 1).set_table_styles(styles).hide_index()
 
-    toti = datetime.datetime.today().date() - timedelta(days = 1)
+    toti = datetime.datetime.today().date() - timedelta(days = 2)
     if toti.weekday() <5:
         toti = "<center><caption>Wednesday, " + str(toti) + "</caption></center>"
     else:
@@ -402,7 +403,7 @@ if not df_new.equals(df_old):
 
     import datetime
     from datetime import timedelta
-    toti = datetime.datetime.today().date() - timedelta(days = 1)
+    toti = datetime.datetime.today().date() - timedelta(days = 2)
     if toti.weekday() <5:
         told = toti - timedelta(days = 3)
         told = "<center><caption>Sunday, " + str(told) + "</caption></center>"
