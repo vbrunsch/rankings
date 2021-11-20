ec40f2aa1d093ff74ca65d9720b2e6944ac80509
vbrunsch <62330755+vbrunsch@users.noreply.github.com>
2021-11-20 18:59:44 -0500

    Update BelgiumDistricts.py

--- Belgium/BelgiumDistricts.py
+++ Belgium/BelgiumDistricts.py
@@ -13,7 +13,7 @@ import pandas as pd
 mun = pd.DataFrame()
 now = datetime.date.today()
 yesterday = now - timedelta(days=1)
-start = now - timedelta(days=20)
+start = now - timedelta(days=17)
 date_list = pd.date_range(start=start,end=yesterday).tolist()
 for d in date_list:  
     if d.weekday() in [1,2,3,4,5]:
