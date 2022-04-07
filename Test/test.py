738a92677f89947ebeda811b3840e779f3e2680f
vbrunsch <62330755+vbrunsch@users.noreply.github.com>
2022-04-07 01:20:11 -0400

    Update test.py

--- Test/test.py
+++ Test/test.py
@@ -1,7 +1,26 @@
-
-import pandas as pd
 import numpy as np
-import geopandas as gpd
+import matplotlib.pyplot as plt
 
-df = gpd.read_file('https://opendata.arcgis.com/datasets/dd4580c810204019a7b8eb3e0b329dd6_0.geojson')
-print(list(df))
+fig, ax = plt.subplots()
+
+# Plot diagonal line (45 degrees)
+h = ax.plot(range(0, 10), range(0, 10))
+
+# set limits so that it no longer looks on screen to be 45 degrees
+ax.set_xlim([-10, 20])
+
+# Locations to plot text
+l1 = np.array((1, 1))
+l2 = np.array((5, 5))
+
+# Rotate angle
+angle = 45
+
+# Plot text
+th1 = ax.text(*l1, 'text not rotated correctly', fontsize=16,
+              rotation=angle, rotation_mode='anchor')
+th2 = ax.text(*l2, 'text rotated correctly', fontsize=16,
+              rotation=angle, rotation_mode='anchor',
+              transform_rotates_text=True)
+
+plt.show()
